e5302561fe2c3c87d07b9c698747d7ac61d32665
TangRufus <tangrufus@gmail.com>
2020-01-16 13:06:46 +0000

    First working version

--- README.md
+++ README.md
@@ -2,31 +2,3 @@
 ====================
 
 HTTP status code checker
-
-[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
-[![Version](https://img.shields.io/npm/v/@itinerisltd/twoohoh.svg)](https://npmjs.org/package/@itinerisltd/twoohoh)
-[![Downloads/week](https://img.shields.io/npm/dw/@itinerisltd/twoohoh.svg)](https://npmjs.org/package/@itinerisltd/twoohoh)
-[![License](https://img.shields.io/npm/l/@itinerisltd/twoohoh.svg)](https://github.com/ItinerisLtd/twoohoh/blob/master/package.json)
-
-<!-- toc -->
-* [Usage](#usage)
-* [Commands](#commands)
-<!-- tocstop -->
-# Usage
-<!-- usage -->
-```sh-session
-$ npm install -g @itinerisltd/twoohoh
-$ twoohoh COMMAND
-running command...
-$ twoohoh (-v|--version|version)
-@itinerisltd/twoohoh/0.0.0 darwin-x64 node-v10.18.1
-$ twoohoh --help [COMMAND]
-USAGE
-  $ twoohoh COMMAND
-...
-```
-<!-- usagestop -->
-# Commands
-<!-- commands -->
-
-<!-- commandsstop -->
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,45 @@
 import {Command, flags} from '@oclif/command'
+import fetch from 'node-fetch'
 
 class ItinerisltdTwoohoh extends Command {
-  static description = 'describe the command here'
+  static description = 'HTTP status code checker'
 
   static flags = {
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
+  static strict = false;
+
+  static args = [
+    {name: 'url', required: true},
+    {name: '...urls'},
+  ]
 
   async run() {
-    const {args, flags} = this.parse(ItinerisltdTwoohoh)
+    await Promise.all(
+      this.argv.map(url => this.is200(url))
+    )
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const count = this.argv.length
+    if (count > 1) {
+      this.log(`Success: All (${this.argv.length}) URLs return 200 okay`)
     }
+  }
+
+  is200(url: string) {
+    return fetch(url)
+    .catch((err: Error) => {
+      this.error(`Fail to retrieve ${url}\n${err.name}: ${err.message}`)
+    })
+    .then(({status, statusText}) => {
+      if (status === 200) {
+        this.log(`${url} returns 200 okay`)
+      } else {
+        this.error(`${url} returns ${status} ${statusText}`)
+      }
+    })
   }
 }
 
